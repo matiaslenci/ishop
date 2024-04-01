abf9ed291035e959a2a6e08847d3c901c7b6c557
Matias Lenci <matiaslenci@gmail.com>
2024-04-02 00:58:52 +0700

    add funcionalidad basica a selectPhone

--- src/components/SelectPhone.tsx
+++ src/components/SelectPhone.tsx
@@ -1,36 +1,30 @@
+import { useState } from "react";
+
 export function SelectPhone() {
-  let selectPro = true;
+  const [selectPro, setSelectPro] = useState(true);
 
-  function toggleSelect(): void {
-    selectPro = !selectPro;
-  }
+  const handleSelect = () => {
+    setSelectPro(!selectPro);
+  };
 
-  /*   document.getElementById("iphone")?.addEventListener("click", function () {
-    toggleSelect();
-  }); */
+  const getButtonClasses = (isSelected: boolean) => {
+    return isSelected
+      ? "text-blue-500 border-4 border-blue-500 cursor-default px-5 py-3 font-semibold text-xl text-center rounded-lg"
+      : "inline-flex items-center justify-center text-black hover:text-white cursor-pointer hover:bg-blue-500 px-5 py-3 font-semibold text-xl text-center rounded-lg";
+  };
 
   return (
     <section className="flex flex-row gap-3">
       <button
-        onClick={() => toggleSelect()}
-        id="iphone"
-        className={
-          selectPro
-            ? "text-blue-500 border-4 border-blue-500 cursor-default px-5 py-3 font-semibold text-xl text-center  rounded-lg "
-            : "inline-flex items-center justify-center text-black  hover:text-white cursor-pointer  hover:bg-blue-500 px-5 py-3 font-semibold text-xl text-center  rounded-lg"
-        }
+        onClick={() => handleSelect()}
+        className={getButtonClasses(selectPro)}
       >
         iPhone 15 Pro Max
       </button>
 
       <button
-        onClick={() => toggleSelect()}
-        id="iphone"
-        className={
-          !selectPro
-            ? "text-blue-500 border-4 border-blue-500 cursor-default px-5 py-3 font-semibold text-xl text-center  rounded-lg "
-            : "inline-flex items-center justify-center text-black  hover:text-white cursor-pointer  hover:bg-blue-500 px-5 py-3 font-semibold text-xl text-center  rounded-lg"
-        }
+        onClick={() => handleSelect()}
+        className={getButtonClasses(!selectPro)}
       >
         iPhone 15
       </button>
